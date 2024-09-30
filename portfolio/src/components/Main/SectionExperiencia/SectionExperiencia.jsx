fb7f8cd423573368ba47ac2261c2067248eca6bd
junior <junior.oliveira05@icloud.com>
2024-09-30 18:32:02 -0300

    refactor(sobre): simplificar e otimizar resumo do projeto no portfólio

--- portfolio/src/components/Main/SectionExperiencia/SectionExperiencia.jsx
+++ portfolio/src/components/Main/SectionExperiencia/SectionExperiencia.jsx
@@ -19,27 +19,27 @@ export function SectionExperiencia() {
     },
     {
       id: 2,
-      titulo: 'Ford',
+      titulo: 'Ford Enter',
       data: "Maio 2024",
-      descricao: "Participei e me formei no Curso de Front-end da Ford Company com uma carga horária de 440 horas, realizado presencialmente no Senai do Ipiranga, na escola Conde José Vicente de Azevedo. O curso focou no desenvolvimento da interface gráfica de sites e aplicações, capacitando-me em habilidades como a criação de áreas de pesquisa e formulários interativos. Utilizei tecnologias como HTML, CSS, JavaScript, React, Bootstrap e Tailwind CSS. Além disso, tive acesso a treinamentos comportamentais, programa de mentoria, curso de inglês técnico e visitas a empresas de tecnologia.."
+      descricao: "Participei como bolsista e me formei no Curso de Front-end da Ford Company com uma carga horária de 440 horas, realizado presencialmente no Senai do Ipiranga, na escola Conde José Vicente de Azevedo. O curso focou no desenvolvimento da interface gráfica de sites e aplicações, capacitando-me em habilidades como a criação de áreas de pesquisa e formulários interativos. Utilizei tecnologias como HTML, CSS, JavaScript, React, Bootstrap e Tailwind CSS. Além disso, tive acesso a treinamentos comportamentais, programa de mentoria, curso de inglês técnico e visitas a empresas de tecnologia.."
     },
     {
       id: 3,
-      titulo: 'vemSerTech',
+      titulo: 'Vem Ser Tech',
       data: "Março 2024",
-      descricao: "Participei como bolsista no Curso de Desenvolvimento Front-End, oferecido pelo iFood em parceria com a AdaTech, com uma carga horária de 324 horas. O curso abordou desde o desenvolvimento de front-end estático até a construção de front-end dinâmico com JavaScript Vanilla. Também incluiu tópicos como lógica de programação e programação orientada a objetos usando JavaScript e TypeScript. Além disso, explorei o uso do framework React, tanto em módulos básicos quanto avançados, proporcionando uma formação completa em desenvolvimento front-end."
+      descricao: "Participei como bolsista e me formei no Curso de Desenvolvimento Front-End, oferecido pelo iFood em parceria com a AdaTech, com uma carga horária de 324 horas. O curso abordou desde o desenvolvimento de front-end estático até a construção de front-end dinâmico com JavaScript Vanilla. Também incluiu tópicos como lógica de programação e programação orientada a objetos usando JavaScript e TypeScript. Além disso, explorei o uso do framework React, tanto em módulos básicos quanto avançados, proporcionando uma formação completa em desenvolvimento front-end."
     },
     {
       id: 4,
-      titulo: 'certificados',
+      titulo: 'Certificados',
       data: "Nov 2024-Atual",
       descricao: "Estou empolgado em compartilhar nossa jornada incrível durante o AdaHack 2024, organizado pela Ada Tech onde a equipe FE-3 se uniu"
     },
     {
       id: 5,
-      titulo: 'sobre',
+      titulo: 'Sobre o portfólio',
       data: "Nov 2024-Atual",
-      descricao: "Estou empolgado em compartilhar nossa jornada incrível durante o AdaHack 2024, organizado pela Ada Tech onde a equipe FE-3 se uniu"
+      descricao: "Desenvolvi o projeto do portfólio utilizando React e Vite, junto com a biblioteca Phosphor Icons para ícones. O projeto foi organizado em componentes como Header, Main e Footer, com cada seção do Main (Nome e Banner, Sobre, Experiência e Projetos) sendo separada e estilizada individualmente para evitar conflitos. Usei um arquivo db.json para armazenar os dados dos projetos e fiz o deploy no Render, simulando uma API que é consumida com Axios. Os dados são exibidos no componente Card, que atualiza seu estado com as informações da API. Também criei uma segunda API para a seção de Experiência, gerenciada em um arquivo separado. Todo o projeto segue boas práticas com commits semânticos."
     }
   ]
 
@@ -63,10 +63,10 @@ export function SectionExperiencia() {
         <div className="container-experiencia">
           <ul>
             <li onClick={() => handleClick('Hackathon')}><a>AdaHack 2024</a></li>
-            <li onClick={() => handleClick('Ford')}><a>Ford Enter</a></li>
-            <li onClick={() => handleClick('vemSerTech')}><a>Vem Ser Tech</a></li>
-            <li onClick={() => handleClick('certificados')}><a>Certificados</a></li>
-            <li onClick={() => handleClick('sobre')}><a>Sobre o portfólio</a></li>
+            <li onClick={() => handleClick('Ford Enter')}><a>Ford Enter</a></li>
+            <li onClick={() => handleClick('Vem Ser Tech')}><a>Vem Ser Tech</a></li>
+            <li onClick={() => handleClick('Certificados')}><a>Certificados</a></li>
+            <li onClick={() => handleClick('Sobre o portfólio')}><a>Sobre o portfólio</a></li>
           </ul>
         </div>
       </div>
